14317d76b093687a1e9d62b85d3351ba6fb45b86
AtishSanyal09 <126420084+AtishSanyal09@users.noreply.github.com>
2023-02-27 03:28:11 +0530

    Update Sentiment analysis.py

--- Sentiment analysis.py
+++ Sentiment analysis.py
@@ -1,7 +1,7 @@
 from textblob import TextBlob
 from newspaper import Article
 
-url ="https://timesofindia.indiatimes.com/city/bengaluru/how-3-bengalureans-helped-sc-script-history-with-technology-tool/articleshow/98241338.cms?from=mdr"
+url ="text"
 article = Article(url)
 article.download()
 article.parse()
